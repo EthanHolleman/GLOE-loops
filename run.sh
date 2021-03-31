877c68fc294abf66920be609c04c1b9a20999b53
Your Name <etholleman@ucdavis.edu>
2021-03-30 20:59:21 -0700

    use conda

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 
 mkdir -p Logs
 conda activate snakemake
-snakemake -j 12 -s /home/ethollem/projects/GLOE-loop/Snakefile --cluster-config /home/ethollem/projects/GLOE-loop/cluster.yaml --cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" --latency-wait 120 -p -k --rerun-incomplete $@
-
+snakemake -j 14 -s /home/ethollem/projects/GLOE-loop/Snakefile --cluster-config \
+/home/ethollem/projects/GLOE-loop/cluster.yaml \
+--cluster "sbatch -p {cluster.partition} -t {cluster.time} -N {cluster.nodes} -n {cluster.cpus} --mem {cluster.mem} -J {cluster.name} -o {cluster.output} -e {cluster.output} --mail-type ALL --mail-user {cluster.email}" \
+--latency-wait 120 --verbose --use-conda -p -k $@
